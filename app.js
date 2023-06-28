c30b03cdefff97571d95c8090c8ca935a7ed3a02
maxilex <61802609+maxilex@users.noreply.github.com>
2023-06-28 20:23:24 +0000

    app2

--- app.js
+++ app.js
@@ -2,16 +2,35 @@ Telegram.WebApp.ready()
 let tg = window.Telegram.WebApp
 
 tg.MainButton.setParams({
-  text: 'Закрыть',
-  is_visible: true
+    text: 'Закрыть',
+    is_visible: true
 }).onClick(Telegram.WebApp.close)
 
 //окно сканирования
 function showScanQr() {
-  tg.showScanQrPopup({
-    text: 'Сканируем qr код'
-  }, function (text) {
-    tg.showAlert(text);
-    return true;
-  });
+    tg.showScanQrPopup({
+        text: 'Сканируем qr код'
+    }, function (text) {
+        tg.showAlert(text)
+        return true
+    })
+}
+
+// Окно опрос с обаботкой результата
+function showPopup() {
+    tg.showPopup({
+        title: 'Заголовок',
+        message: 'Текст сообщения',
+        buttons: [
+            { id: 'delete', type: 'destructive', text: 'Delete all' },
+            { id: 'faq', type: 'default', text: 'Open FAQ' },
+            { type: 'cancel' },
+        ]
+    }, function (button_id) {
+        if (button_id == 'delete') {
+            tgweb.showAlert("'Delete all' selected")
+        } else if (button_id == 'faq') {
+            tg.openLink('https://telegram.org/faq')
+        }
+    })
 }
